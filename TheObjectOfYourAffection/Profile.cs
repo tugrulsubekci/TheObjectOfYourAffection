fd3dc1815f20caa1557703473262904502dfe94a
Tuğrul Sübekci <tugrulsubekci@gmail.com>
2022-05-25 21:11:37 +0300

    Add project files.

---
+++ TheObjectOfYourAffection/Profile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheObjectOfYourAffection
+{
+    internal class Profile
+    {
+        //fields
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        // Constructors
+        public Profile(string name, int age, string city, string country, string pronouns)
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+        }
+
+        // Propeties
+
+
+
+        //methods
+
+        public string ViewProfile()
+        {
+            string profileInformations = $"Name: {name}\nAge: {age}\nCity: {city}\nCountry: {country}\nPronouns: {pronouns}\nHobbies: {hobbies}";
+            
+            profileInformations += "\nHobbies:\n";
+
+            foreach (string hobby in hobbies)
+            {
+                profileInformations += $"-{hobby}\n";
+            }
+
+            return profileInformations;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies=hobbies;   
+        }
+    }
+}
---
+++ TheObjectOfYourAffection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheObjectOfYourAffection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+
+            sam.SetHobbies(new string[] { "listening to audiobooks and podcasts", "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice columns" });
+
+            Console.WriteLine(sam.ViewProfile());
+            
+
+            // to keep console open
+            Console.ReadLine();
+        }
+    }
+}
